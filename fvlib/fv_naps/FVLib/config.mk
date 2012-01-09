30ade4b62942c00dfcded01cfad8da42450b1c0d
Miguel Palhas <mpalhas@gmail.com>
2012-01-09 00:29:53 +0000

    fv_naps added

---
+++ fvlib/fv_naps/FVLib/config.mk
@@ -0,0 +1,28 @@
+MODE	=	RELEASE
+
+#	C++ Compil[ator]
+CXX		=	g++
+
+#	Include directories
+INC		=	-I $(ROOT)/include
+
+#	Compiler flags
+CXXFLAGS	=	-Wall \
+				-Wextra \
+				-Wfatal-errors \
+				-ansi \
+				-pedantic \
+				$(INC)
+ifeq ($(MODE),DBG)
+CXXFLAGS	+=	-g
+SUFFIX=_$(MODE)
+else ifeq ($(MODE),GPROF)
+CXXFLAGS	+=	-pg 
+SUFFIX=_$(MODE)
+else
+CXXFLAGS	+=	-O3
+SUFFIX=
+endif
+
+#	Linker flags
+LDFLAGS	=	-L $(ROOT)/lib -lFVLib
---
+++ fvlib/fv_naps/FVLib/lib/README.txt
@@ -0,0 +1,1 @@
+The libFVLib.a repertory
