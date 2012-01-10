728ac68ea06528143da404e30fa97d48169d8a50
Pedro Costa <dev@pfac.info>
2012-01-10 22:05:28 +0000

    PMake: Patra Makefiles!

---
+++ fvlib/fv_custom_pfac/conf/src.mk
@@ -0,0 +1,6 @@
+include $(ROOTD)/conf/config.mk
+
+SRCS		=	$(shell find . -type f | egrep -o "[[:alnum:]\-_.:]+(\.cpp|\.cc)")
+SRCS.cpp	=	$(filter %.cpp,$(SRCS))
+SRCS.cc		=	$(filter %.cc,$(SRCS))
+OBJS		= 	$(SRCS.cpp:%.cpp=%.o) $(SRCS.cc:%.cc=%.o)
