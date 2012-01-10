942191d9b08b98c5638ada28e243f891eae169f5
Miguel Palhas <mpalhas@gmail.com>
2012-01-10 01:07:54 +0000

    added fv_cuda

---
+++ fvlib/fv_cuda/FVLib/include/FVEdge3D.h
@@ -0,0 +1,38 @@
+#ifndef __FVEDGE3D_H
+#define __FVEDGE3D_H
+#include <vector>
+#include "FVPoint3D.h"
+#include "FVLib_config.h"
+using namespace std;
+
+
+class FVVertex3D;
+class FVEdge3D
+{
+public:
+public:
+FVPoint3D<double> centroid;
+double length;
+size_t label, code,nb_vertex;
+FVVertex3D *firstVertex,*secondVertex; // the two vertices
+     FVEdge3D(){firstVertex=NULL;secondVertex=NULL;nb_vertex=0;label=0;}
+    ~FVEdge3D(){;} 
+void setCode2Vertex(size_t val=0)
+         {if (firstVertex) firstVertex->code=val;if(secondVertex) secondVertex->code=val;} 
+    
+private:
+
+};
+
+
+
+inline bool isEqual(FVEdge3D *e1, FVEdge3D *e2)
+     {
+      bool is_equal1 = false, is_equal2 = false;  
+      if(e1->firstVertex->label==e2->firstVertex->label) is_equal1=true;   
+      if(e1->firstVertex->label==e2->secondVertex->label) is_equal1=true;
+      if(e1->secondVertex->label==e2->firstVertex->label) is_equal2=true;
+      if(e1->secondVertex->label==e2->secondVertex->label) is_equal2=true;
+      return(is_equal1 && is_equal2);
+     }
+#endif // define _FVEDGE3D
