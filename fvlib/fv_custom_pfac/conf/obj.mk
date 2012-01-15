5888b0bebeca40499989413bed739482e590d478
Pedro Costa <dev@pfac.info>
2012-01-15 00:56:47 +0000

    Starting PAPI

--- fvlib/fv_custom_pfac/conf/obj.mk
+++
@@ -1,3 +0,0 @@
-include $(ROOTD)/conf/config.mk
-
-OBJS		=	$(shell find . -type f | egrep -o "[[:alnum:]_.:\-]+\.o$$")
--- fvlib/fv_default/FVLib/include/FVGlobal.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FVGLOBAL 
-#define _FVGLOBAL
-
-static unsigned int nb_thread=1;  // number of threads using by openMP 
-extern unsigned int nb_thread;
-#endif // define _FVGLOBAL
