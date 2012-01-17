80495e56677a067ec141c583a00bb4742c18b41b
Pedro Costa <dev@pfac.info>
2012-01-17 11:15:24 +0000

    PAPI w/ OpenMP: thread oriented measures test

--- src/papi.openmp/main.cpp
+++ src/papi.openmp/main.cpp
@@ -21,13 +21,16 @@ int main ()
 	int t;//	current thread
 	long long int sum, prd;
 	long long int *sums, *prds;
+	long long int *tot_ins_v, *tot_cyc_v;
+	long long int tot_ins, tot_cyc;
 	unsigned i;
-	PAPI p;
 
 	min = INT_MAX;
 	max = INT_MIN;
 	sum = 0;
 	prd = 1;
+	tot_ins = 0;
+	tot_cyc = 0;
 
 	srand( time(NULL) );
 
@@ -39,10 +42,7 @@ int main ()
 			<<	endl;
 	}
 
-	p.add_event( PAPI_TOT_INS );
-	p.add_event( PAPI_LD_INS );
-
-	p.start();
+	PAPI::init();
 
 	#pragma omp parallel default(shared) private(t)
 	{
@@ -53,7 +53,11 @@ int main ()
 			prds = new long long int[ tc ];
 			mins = new int[ tc ];
 			maxs = new int[ tc ];
+			tot_ins_v = new long long int[ tc ];
+			tot_cyc_v = new long long int[ tc ];
 		}
+
+		PAPI p;
 
 		t = omp_get_thread_num();
 
@@ -61,6 +65,11 @@ int main ()
 		prds[t] = 1;
 		mins[t] = INT_MAX;
 		maxs[t] = INT_MIN;
+
+		p.add_event( PAPI_TOT_INS );
+		p.add_event( PAPI_TOT_CYC );
+
+		p.start();
 
 		#pragma omp for
 		for (i = 0; i < ARRAY_SIZE; ++i)
@@ -70,6 +79,11 @@ int main ()
 			mins[t] = ( array[i] < mins[t] ) ? array[i] : mins[t];
 			maxs[t] = ( array[i] > maxs[t] ) ? array[i] : maxs[t];
 		}
+
+		p.stop();
+
+		tot_ins_v[t] = p[ PAPI_TOT_INS ];
+		tot_cyc_v[t] = p[ PAPI_TOT_CYC ];
 	}
 
 	for (t = 0; t < tc; ++t)
@@ -78,17 +92,19 @@ int main ()
 		prd *= prds[t];
 		min = ( mins[t] < min ) ? mins[t] : min;
 		max = ( maxs[t] > max ) ? maxs[t] : max;
+		tot_ins += tot_ins_v[t];
+		tot_cyc += tot_cyc_v[t];
 	}
-	
-	p.stop();
 
 	cout
 		<<	"sum: "	<<	sum	<<	endl
 		<<	"prd: "	<<	prd	<<	endl
 		<<	"max: "	<<	max	<<	endl
 		<<	"min: "	<<	min	<<	endl
-		<<	"PAPI_TOT_INS: "	<<	p[ PAPI_TOT_INS ]	<< endl
-		<<	"PAPI_LD_INS: "	<<	p[ PAPI_LD_INS	]	<<	endl;
+		<<	"PAPI_TOT_INS: "	<<	tot_ins	<< endl
+		<<	"PAPI_TOT_CYC: "	<<	tot_cyc	<<	endl;
+	
+	PAPI::shutdown();
 
 	return 0;
 }
