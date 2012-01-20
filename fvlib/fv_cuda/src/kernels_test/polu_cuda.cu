b398759ccc4d6bd56b1e087d618c67faa505fb8d
Miguel Palhas <mpalhas@gmail.com>
2012-01-20 03:17:47 +0000

    cuda

--- fvlib/fv_cuda/src/kernels_test/polu_cuda.cu
+++ fvlib/fv_cuda/src/kernels_test/polu_cuda.cu
@@ -10,7 +10,7 @@
 int main() {
 
 	int test[11];
-	int result[2];
+	int result[3];
 	int *d_test, *d_result;
 
 	for(int i=0; i < 8; ++i)
@@ -22,7 +22,7 @@ int main() {
 	result[0] = 62;
 	result[1] = 25;
 
-	dim3 numBlocks=2;
+	dim3 numBlocks=3;
 	dim3 numThreads=4;
 
 	cudaMalloc(&d_test, sizeof(int)*8);
@@ -34,7 +34,7 @@ int main() {
 		cout << result[i] << endl;
 	}
 
-	kernel_velocities_reduction<<< numBlocks, numThreads >>>(8, d_test, d_result);
+	kernel_velocities_reduction<<< numBlocks, numThreads >>>(11, d_test, d_result);
 
 	cudaMemcpy(result, d_result, sizeof(int)*2, cudaMemcpyDeviceToHost);
 	cout << "after: " << endl;
