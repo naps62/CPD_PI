b7b6f7def3ae08defd1f8ffbedbb5d6df90297ad
Miguel Palhas <mpalhas@gmail.com>
2012-01-21 03:43:02 +0000

    cuda update

--- fvlib/fv_cuda/src/polu.cuda/kernels.cu
+++ fvlib/fv_cuda/src/polu.cuda/kernels.cu
@@ -274,13 +274,13 @@ void kernel_update(
 		// if this cell is at the left of the edge
 
 		//double aux_polution = dt;//dt * flux[edge] * edge_lengths[edge] / cell_areas[tid];
-		//if (edge_left_cells[edge] == tid) {
-		//	new_polution -= aux_polution;
+		if (edge_left_cells[edge] == tid) {
+			new_polution += 1;
 		//}// else if (edge_right_cells[edge] == tid){ //otherwise, this cell is obviosly to the right of the edge
 		//	new_polution += aux_polution;
-		//} else {
-			new_polution = 50;
-		//}
+		} else {
+			new_polution += 10;
+		}
 	}
 
 	// update global value
