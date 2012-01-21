9fc1ca7991eabc89c22408376ce219fa493c81b8
Miguel Palhas <mpalhas@gmail.com>
2012-01-21 02:34:22 +0000

    cuda reduction

--- fvlib/fv_cuda/src/polu.cuda/kernels.cu
+++ fvlib/fv_cuda/src/polu.cuda/kernels.cu
@@ -272,9 +272,9 @@ void kernel_update(
 	for(unsigned int i = edge_index; i < edge_limit; ++i) {
 		// if this cell is at the left of the edge
 		if (edge_left_cells[i] == tid) {
-			new_polution += dt * flux[i] * edge_lengths[i] / cell_areas;
+			new_polution += dt * flux[i] * edge_lengths[i] / cell_area;
 		} else { //otherwise, this cell is obviosly to the right of the edge
-			new_polution -= dt * flux[i] * edge_lengths[i] / cell_areas;
+			new_polution -= dt * flux[i] * edge_lengths[i] / cell_area;
 		}
 	}
 
