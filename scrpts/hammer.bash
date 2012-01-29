1ae0f96ebf7db6683a51afda77e418a12ba16256
Pedro Costa <dev@pfac.info>
2012-01-29 04:58:42 +0000

    Hammer the results in

---
+++ scrpts/hammer.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare -a CASES
+
+CASES=( tiny small medium big huge )
+COUNTERS=( cpi mem flops l1 l2dca ipb mbaml mbadv )
+
+for c in ${CASES[@]};
+do
+	for counter in ${COUNTERS[@]};
+	do
+		timestamp=`date "+%y-%m-%d_%H:%M:%S"`
+		echo "${c} - ${counter}";
+		bin/polu.openmp.papi.${counter} data/xml/${c}.param.xml > results/${c}.${counter}.${timestamp}.hammer";
+	done;
+done;
+
+
