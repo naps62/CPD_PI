8ff820d30248ac1ddaf0aba8183bdb2a057e7d3d
Pedro Costa <dev@pfac.info>
2012-01-30 18:17:14 +0000

    101 nodes suckvim

--- scrpts/pbs.511.bash
+++ scrpts/pbs.511.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 #	SeARCH Job Script
-#	Runs OpenMP on the 2 core with 2GB RAM Xeon 3.2GHz (101 group)
+#	Runs OpenMP on the 24 core with 64GB RAM AMD Opt 6174 (group 511)
 #
-#PBS -l nodes=1:r101
+#PBS -l nodes=1:r511
 #PBS -l walltime=1:00:00
 #
 #PBS -M pdrcosta90@gmail.com
 #PBS -m bea
-#PBS -e out/openmp.101.err
-#PBS -o out/openmp.101.out
+#PBS -e out/openmp.511.err
+#PBS -o out/openmp.511.out
 #
 CASES=( tiny small medium huge original )
 EXE="polu.openmp"
