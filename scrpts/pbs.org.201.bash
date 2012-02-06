311eefce376fbcb0506bf4340344c9ca49f73294
Pedro Costa <dev@pfac.info>
2012-02-06 14:53:58 +0000

    Walltime exceeded retests

--- scrpts/pbs.org.201.bash
+++ scrpts/pbs.org.201.bash
@@ -4,15 +4,17 @@
 #	Runs OpenMP on the 4 core with 4GB RAM Intel Xeon 5130 (group 201)
 #
 #PBS -l nodes=1:r201:ppn=4
-#PBS -l walltime=5:00:00
+#PBS -l walltime=3:00:00
 #
 #PBS -M pdrcosta90@gmail.com
 #PBS -m bea
-#PBS -e out/org.201.err
-#PBS -o out/org.201.out
+#PBS -e out/org.201-2.err
+#PBS -o out/org.201-2.out
 #
-CASES=( "tiny" "small" "medium" "big" "huge" )
-TIMERS=( "main" "iteration" "functions" )
+#CASES=( "tiny" "small" "medium" "big" "huge" )
+CASES=( "huge" );
+#TIMERS=( "main" "iteration" "functions" )
+TIMERS=( "iteration" "functions" )
 EXE="polu.orig.time"
 
 cd "$PBS_O_WORKDIR"
--- scrpts/pbs.org.511.bash
+++ scrpts/pbs.org.511.bash
@@ -4,15 +4,17 @@
 #	Runs OpenMP on the 24 core with 64GB RAM AMD Opt 6174 (group 511)
 #
 #PBS -l nodes=1:r511:ppn=24
-#PBS -l walltime=5:00:00
+#PBS -l walltime=3:00:00
 #
 #PBS -M pdrcosta90@gmail.com
 #PBS -m bea
-#PBS -e out/org.511.err
-#PBS -o out/org.511.out
+#PBS -e out/org.511-2.err
+#PBS -o out/org.511-2.out
 #
-CASES=( "tiny" "small" "medium" "big" "huge" )
-TIMERS=( "main" "iteration" "functions" )
+#CASES=( "tiny" "small" "medium" "big" "huge" )
+CASES=( "huge" );
+#TIMERS=( "main" "iteration" "functions" )
+TIMERS=( "functions" );
 EXE="polu.orig.time"
 
 cd "$PBS_O_WORKDIR"
