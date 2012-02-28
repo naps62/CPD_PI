26b2000ee8a23689642113b2f430f8e2c8d18358
Pedro Costa <dev@pfac.info>
2012-02-28 18:02:00 +0000

    Computational intensity first test

--- src/libpapipcc/computational_intensity.cpp
+++ src/libpapipcc/computational_intensity.cpp
@@ -1,16 +1,12 @@
 #include "papi/computational_intensity.hpp"
 
-#define BUS_TRANS_MEM        0x40000114
-#define INSTRUCTIONS_RETIRED 0x40000001
-#define INST_RETIRED_LOADS   0x4000000e
-#define INST_RETIRED_STORES  0x4000000f
+#define BUS_TRANS_MEM      0x40000114
+#define INST_RETIRED_OTHER 0x4000015f
 
 PAPI_ComputationalIntensity::PAPI_ComputationalIntensity () : PAPI_Preset ()
 {
 	this->add_event( BUS_TRANS_MEM );
-	this->add_event( INSTRUCTIONS_RETIRED );
-	this->add_event( INST_RETIRED_LOADS );
-	this->add_event( INST_RETIRED_STORES );
+	this->add_event( INST_RETIRED_OTHER );
 }
 
 long long int PAPI_ComputationalIntensity::ram_accesses ()
@@ -25,7 +21,7 @@ long long int PAPI_ComputationalIntensity::bytes_accessed ()
 
 long long int PAPI_ComputationalIntensity::instructions ()
 {
-	return (*this)[ INSTRUCTIONS_RETIRED ] - (*this)[ INST_RETIRED_LOADS ] - (*this)[ INST_RETIRED_STORES ];
+	return (*this)[ INST_RETIRED_OTHER ];
 }
 
 double PAPI_ComputationalIntensity::intensity ()
