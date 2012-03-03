345f5be0cb2f04fa71fe8288add3bc3d5ec763ff
Pedro Costa <dev@pfac.info>
2012-03-03 17:50:07 +0000

    Scripts

---
+++ scrpts/auto.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+bin/polu.optim.bytes data/xml/tiny.param.xml > results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+bin/polu.optim.bytes data/xml/tiny.param.xml >> results/tiny.out
+
+bin/polu.optim.bytes data/xml/small.param.xml > results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+bin/polu.optim.bytes data/xml/small.param.xml >> results/small.out
+
+bin/polu.optim.bytes data/xml/medium.param.xml > results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+bin/polu.optim.bytes data/xml/medium.param.xml >> results/medium.out
+
+bin/polu.optim.bytes data/xml/big.param.xml > results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+bin/polu.optim.bytes data/xml/big.param.xml >> results/big.out
+
+bin/polu.optim.bytes data/xml/huge.param.xml > results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
+bin/polu.optim.bytes data/xml/huge.param.xml >> results/huge.out
--- scrpts/sampler.plx
+++ scrpts/sampler.plx
@@ -4,9 +4,9 @@ use warnings;
 @cases = ("tiny","small","medium","big","huge");
 $samples = 10;
 
-for ( $i = 0 ; $i < $samples ; ++$i )
+foreach $case ( @cases )
 {
-	foreach $case ( @cases )
+	for ( $i = 0 ; $i < $samples ; ++$i )
 	{
 #		system( "echo" , $i, "bin/polu.optim.bytes" , "data/xml/$case.param.xml" );
 		system( "bin/polu.optim.bytes" , "data/xml/$case.param.xml" );
