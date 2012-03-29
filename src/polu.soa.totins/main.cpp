e00420f27426bddbac35625316ae07cab7b4f3fe
Pedro Costa <dev@pfac.info>
2012-03-29 23:06:15 +0100

    Maybe it's now...

---
+++ src/polu.soa.totins/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <limits>
+
+#include <papi/instruction.hpp>
+
+
+
+#define PROFILE_COUNTER_CLASS papi::counters::TotalInstructionsCounter
+#define PROFILE_COUNTER_NAME  p
+#define PROFILE_COUNTER_FIELD instructions
+#define PROFILE_COUNTER       profile::PROFILE_COUNTER_NAME
+
+
+
+namespace profile
+{
+	PROFILE_COUNTER_CLASS PROFILE_COUNTER_NAME;
+	long long int PROFILE_COUNTER_FIELD;
+
+	long long int cftotns;
+	long long int cfminns;
+	long long int cfmaxns;
+
+	long long int uptotns;
+	long long int upminns;
+	long long int upmaxns;
+
+	struct Overhead
+	{
+		long long int PROFILE_COUNTER_FIELD;
+		long long int nanoseconds;
+	} overhead;
+
+	void init()
+	{
+		PROFILE_COUNTER_FIELD = 0;
+		
+		PROFILE_COUNTER_NAME.start();
+		PROFILE_COUNTER_NAME.stop();
+		overhead.PROFILE_COUNTER_FIELD = PROFILE_COUNTER_NAME.PROFILE_COUNTER_FIELD();
+		overhead.nanoseconds = PROFILE_COUNTER_NAME.last();
+
+		cftotns = 0;
+		cfminns = std::numeric_limits<long long int>::max();
+		cfmaxns = std::numeric_limits<long long int>::min();
+
+		uptotns = 0;
+		upminns = std::numeric_limits<long long int>::max();
+		upmaxns = std::numeric_limits<long long int>::min();
+	}
+
+	void output(std::ostream& out)
+	{
+		out
+			<<	PROFILE_COUNTER_FIELD	<<	';'
+			<<	cftotns	<<	';'
+			<<	cfminns	<<	';'
+			<<	cfmaxns	<<	';'
+			<<	uptotns	<<	';'
+			<<	upminns	<<	';'
+			<<	upmaxns	<<	';'
+								<<	std::endl
+			;
+	}
+}
+
+namespace profile
+{
+	inline
+	void compute_flux()
+	{
+		PROFILE_COUNTER_FIELD += PROFILE_COUNTER_NAME.PROFILE_COUNTER_FIELD() - overhead.PROFILE_COUNTER_FIELD;
+		long long int timens = PROFILE_COUNTER_NAME.last();
+		cftotns += timens;
+		cfminns = ( timens < cfminns ) ? timens : cfminns;
+		cfmaxns = ( timens > cfmaxns ) ? timens : cfmaxns;
+	}
+
+	inline
+	void update()
+	{
+		PROFILE_COUNTER_FIELD += PROFILE_COUNTER_NAME.PROFILE_COUNTER_FIELD() - overhead.PROFILE_COUNTER_FIELD;
+		long long int timens = PROFILE_COUNTER_NAME.last();
+		uptotns += timens;
+		upminns = ( timens < upminns ) ? timens : upminns;
+		upmaxns = ( timens > upmaxns ) ? timens : upmaxns;
+	}
+}
+
+
+
+
+#define PROFILE_INIT() profile::init()
+
+#define PROFILE_RETRIEVE_CF() profile::compute_flux()
+
+#define PROFILE_RETRIEVE_UP() profile::update()
+
+#define PROFILE_OUTPUT() profile::output(std::cout)
+
+#define PROFILE_CLEANUP()
+
+#include "../polu.soa.papi/main.cpp"
