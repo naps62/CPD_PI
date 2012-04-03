1afcb7803dfba696ef0873034adaf8197e8a6876
Miguel Palhas <naps62@naps62debian.home>
2012-04-03 10:12:30 +0100

    deleted some old stuff. why was this still here\!

--- fvlib/fv_custom_pfac/include/FVGlobal.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FVGLOBAL 
-#define _FVGLOBAL
-
-//static unsigned int nb_thread=1;  // number of threads using by openMP 
-//extern unsigned int nb_thread;
-//
-#define UNUSED(x) (void)(x)
-#endif // define _FVGLOBAL
