d57749ac96a93ec5d80e6069bcc1ba2ae01a54c5
Pedro Costa <dev@pfac.info>
2012-04-09 02:43:50 +0100

    No more black magic

---
+++ scrpts/pbs.aos.omp.fnc.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#	SeARCH Job Script
+#
+#PBS -l nodes=1:r201:ppn=4
+#PBS -l walltime=2:00:00
+#
+#PBS -M pdrcosta90@gmail.com
+#PBS -m bea
+#PBS -e data/out/pbs.aos.omp.fnc.err
+#PBS -o data/out/pbs.aos.omp.fnc.out
+#
+declare -a CASES;
+declare -a MEASURES;
+
+CASE="huge"
+EXEC="polu.aos"
+TIMER="omp.fnctime"
+RUNS=10
+
+cd "$PBS_O_WORKDIR";
+
+for (( i = 0 ; i < $RUNS ; ++i ));
+do
+	bin/${EXEC}.${TIMER} data/xml/${CASE}.param.xml;
+done;
