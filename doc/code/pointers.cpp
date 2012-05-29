c15177354e4a49579b9f245bb48a82f0a0eeb303
Pedro Costa <dev@pfac.info>
2012-05-29 09:49:58 +0100

    Merged documentation and reports into master branch

---
+++ doc/code/pointers.cpp
@@ -0,0 +1,5 @@
+  if(ptr_e->rightCell) 
+        {
+        VR=V[ptr_e->rightCell->label-1];
+        polR=pol[ptr_e->rightCell->label-1];
+        }
---
+++ doc/code/wdependence.cpp
@@ -0,0 +1,1 @@
+          if (abs(v)*dt>1) dt=1./abs(v);
